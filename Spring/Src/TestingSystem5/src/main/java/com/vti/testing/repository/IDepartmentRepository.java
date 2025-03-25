0c87382399bea88e4af35a57101070fb91d49600
PhamXuanTien2004 <2221050418@student.humg.edu.vn>
2025-03-25 20:45:19 +0700

    change findByTotalMemberBetween with @query

--- Spring/Src/TestingSystem5/src/main/java/com/vti/testing/repository/IDepartmentRepository.java
+++ Spring/Src/TestingSystem5/src/main/java/com/vti/testing/repository/IDepartmentRepository.java
@@ -2,11 +2,12 @@ package com.vti.testing.repository;
 
 import com.vti.testing.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface IDepartmentRepository extends JpaRepository<Department, Integer>{
     List <Department> findByNameContaining (String name);
-
+    @Query("FROM Department WHERE totalMember BETWEEN ?1 AND ?2")
     List<Department> findByTotalMemberBetween(int from, int to);
 }
